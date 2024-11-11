57f1d5c482ae3e11e91f7a335575c957315cdc0b
Dipali <dipalibohara500@gmail.com>
2024-11-11 07:57:25 -0800

    array

--- arrays/best_time_to_buy_&_sell_stock_I.js
+++ arrays/best_time_to_buy_&_sell_stock_I.js
@@ -20,7 +20,7 @@ var maxProfit = function(prices) {
 };
 
 // Optimal approach:
-// initialize 'minPrice' with the first element of the `prices` array.
+// initialize 'minPrice' with the first element of the `prices` array which will be our buying price.
 // initialize 'maxProfit' to 0 to keep track of the maximum profit.
 // iterate through the `prices` array:
 // for each price, update `minPrice` if the current price is lower.
