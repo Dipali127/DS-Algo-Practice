7b22fe1e185792b1e8e2cec13060636cc37f519e
Dipali Bohara <dipalibohara500@gmail.com>
2025-01-17 00:11:09 +0530

    sliding window

--- sliding_window/longest_substring_without_repeating_character.js
+++ sliding_window/longest_substring_without_repeating_character.js
@@ -1,24 +1,27 @@
 // Leetcode Problem:- 3
 // Brute force approach:
 // approach:-
-// consider each possible substring from string 's'.
-// for each substring, create a set to store each character of the current substring.
-// while storing the characters of the current substring into the set, check if the current character doesn't exist in the set.
-// if it doesn't exist, add it to the set and find the length of the longest substring by using set.size, as the set only stores unique characters.
-// if the character already exists in the set, break the inner loop since we've encountered a duplicate character.
-// after iterating through all possible substrings, return the length of the longest substring.
-// TC:- O(N^2)
-// Explanation:
-// O(N): to iterate through each character for considering each possible substring.
-// O(N): to store each character of the current substring into the set.
-// overall, TC: O(N) + O(N) = O(N^2).
-// SC:- O(N), as we use a set to store characters, and in the worst case, the given string may contain unique characters.
+// Consider each possible substring and for each substring iterate through each them and store their characters in hash set.
+// while iterating throguh current substring if duplicate character found then break loop.
+// After breaking, compare the current substring's length (size of the Set) with the previously stored substring length (`longestSubstring`).
+// If the current length is greater, update `longestSubstring`.
+// Finally, return the value of `longestSubstring`, which holds the maximum length of a substring without repeating characters.
+// Time Complexity: O(N^2), as i have use of nested loops to consider all possible substrings.
+// Space Complexity: O(N), to store the current substring characters in a hash set.
 
+// Note:- 
+// Use -Infinity if you want to handle cases where you might not find any valid substrings and need to guarantee that the first valid 
+// substring will always update the initial value.
+// Use 0 if you are confident that a valid substring will always be found, and you just want a clean, simple starting point.
+// so in the longest substring problem i will always found the longest subtring
+// In the "Longest Substring Without Repeating Characters" problem, you will always find a valid substring (at least one character).
+// So, use initial value of longestSubstring is 0.
+ 
 var lengthOfLongestSubstring = function (string) {
     if (string == "") {
         return 0;
     }
-    let longestSubstring = -Infinity;
+    let longestSubstring = 0;
     for (let i = 0; i < string.length; i++) {
         let set = new Set();
         for (let j = i; j < string.length; j++) {
@@ -34,18 +37,17 @@ var lengthOfLongestSubstring = function (string) {
     return longestSubstring;
 }
 
-// Optimal approach: using sliding window with Two Pointers and set
-// approach:-
-// extend the window by moving the pointer 'j' toward right of given string 's' meanwhile store character of current window
-// in set which keeps track of unique characters.
-// if the character at `j` is already in the set (indicating a duplicate), move the left pointer `i` to the right
-// and remove the character at `i` from the set until the duplicate is removed.
-// if the character at `j` is not in the set, add it to the set and update `longestSubstring` with the size of the current window using `set.size`.
-// continue expanding the window and updating the set and `longestSubstring` until `j` reaches the end of the string.
-// after iterating through the entire string, return the length of the longest substring found without repeating characters.
-// TC:- O(N), as we traverse the string once with the right pointer `j`, and each character is added and removed from the set at most once,
-// making the time complexity linear.
-// SC:- O(N), as the set is used to store the unique characters of the substring, which in the worst case can be as large as the string itself.
+// Optimal approach: use sliding window approach using two pointer i.e slow and fast and hash set.
+// approach:
+// Instead of considering each possible substring explicitly, I will use two pointers, `start` and `end` to represent a sliding window.
+// using the `end` pointer expand the window by iterating through the string. while iterating through the current substring, add the current character
+// to the Set if it doesn't exist in the hash Set, and update the `longSubstring` variable with the maximum length.
+// but If a character is already present in the Set, it means a duplicate element has been found. In this case, I will shrink the window
+// by moving the `start` pointer to the right, removing the character pointed to by `start` from the Set.
+// and continues this process until the `end` pointer has iterated through the entire string.
+// Finally, I will return the length of the longest substring without repeating characters.
+// Time Complexity: O(N), where N is the length of the input string as each character is processed at most twice.
+// Space Complexity: O(N), as we store unique characters in a Set.
 
 var lengthOfLongestSubstring = function (s) {
     if(s === ""){
