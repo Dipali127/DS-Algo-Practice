ee5c94061d195d9f45db3dd3d0bf9b837cf87345
Dipali Bohara <dipalibohara500@gmail.com>
2025-02-10 21:02:06 +0530

    binary search

--- binary_search/find_minimum_in_rotated_sorted_array.js
+++ binary_search/find_minimum_in_rotated_sorted_array.js
@@ -30,44 +30,28 @@ var findMin = function (nums) {
     return min;
 };
 
-// Optimal approach 2: Using Binary Search
-// Approach:
-// use two pointers, 'low' and 'high', are initialized to point to the start and end of the array, respectively.
-// First, checks if the array is already sorted (i.e., if the first element is less than or equal to the last element). 
-// If true, it directly returns the first element since the array is sorted, and the minimum element is at the first index of array.
-// If the array is not sorted, enter inside while loop and calculate the 'mid' index.
-// then, check the following conditions:-
-//  - If the element at 'mid' is smaller than its previous element, then 'nums[mid]' is the minimum, so we return it.
-//  - If the element at 'mid' is greater than its next element, then 'nums[mid + 1]' is the minimum, so we return it.
-//  - If the left part of the array (from 'low' to 'mid') is sorted, the minimum must be in the right part, so we move 'low' to 'mid + 1'.
-//  - If the right part of the array (from 'mid' to 'high') is sorted, the minimum must be in the left part, so we move 'high' to 'mid - 1'.
-// This process continues until we find the minimum element.
-// TC:- O(log N), where 'N' is the number of elements in the array and we in the array, as we only traverse through one half of the array, reducing the search space.
-// SC:- O(1), as no extra space is used except for pointers.
-
-// Note:- WHY MINIMUM VALUE WILL BE NOT IN SORTED PART:-
-// The sorted part of the rotated array only contains elements larger than the minimum because the array has been "rotated."
-// When one part of the array is sorted, the minimum must be in the unsorted part because the rotation has shifted the minimum to that unsorted section. 
-var findMin = function (nums) {
-  let low = 0;
-  let high = nums.length - 1;
-  let mid;
-  if (nums[low] <= nums[high]) {
-    return nums[low];
-  }
-  while (low <= high) {
-    mid = Math.floor(low + (high - low) / 2);
-    if (nums[mid] < nums[mid - 1]) {
-      return nums[mid];
-    }
-    else if (nums[mid] > nums[mid + 1]) {
-      return nums[mid + 1];
-    } else if (nums[low] <= nums[mid]) {
-      low = mid + 1;
-    } else if (nums[mid] <= nums[high]) {
-      high = mid - 1;
-    }
+// optimal approach2:-
+// approach:
+// i will use of binary search to find the minimum value in the nums array.  
+// i will use two pointers, low and high, where low is initialized with 0 and high is initialized
+// with the last index of the nums array.  
+// i will run a while loop until low is less than high. First, i will find the mid index and then check if the value at mid is greater than the value at high.  
+// If nums[mid] > nums[high], it means the left part of the array is already sorted, and the minimum value must be in the right part of mid because the given array nums is right rotated and intially array is in ascending order.  So, I will update low to mid + 1.  
+// Otherwise, if nums[mid] <= nums[high], it means the right part of the array is not sorted, and it might be possible that the value pointed by mid itself is the minimum among all the values in the array, or the minimum value could be in the left part of mid. So, I will update high to mid.  
+// Once low is greater than or equal to high, the loop ends, and the value pointed by low has the minimum value.  
+// I will return nums[low] because it might be possible that high moves before low, and the element before low has a greater value Thus, nums[low] is the minimum element.  
+// TC: O(log N), as only half of the array is iterated in each step.  
+// SC: O(1), since no additional space is used.  
+var findMin = function(nums){
+  let low = 0, high = nums.length - 1;
+  while(low < high){
+     let mid = Math.floor(low + (high - low)/2);
+     if(nums[mid] > nums[high]){
+      low = mid+1;
+     }else{
+      high = mid;
+     }
   }
 
-  return -1;
+  return nums[low]; 
 }
