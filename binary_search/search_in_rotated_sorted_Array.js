9dffd1e251c65f611c7e2834242ac708194267c2
Dipali Bohara <dipalibohara500@gmail.com>
2025-02-10 21:19:47 +0530

    binary search

--- binary_search/search_in_rotated_sorted_Array.js
+++ binary_search/search_in_rotated_sorted_Array.js
@@ -20,19 +20,22 @@ var search = function(nums, target) {
 
 // Optimal Approach: Using Binary Search on a Rotated Sorted Array
 // Approach:
-// use of two pointers 'low' and 'high' to perform binary search.
-// While 'low <= high', calculate the 'mid' index to check if the target is at mid.
-// if the target is found at 'mid', return the mid index immediately.
-// if not then determine which part of the array is sorted (left or right).
-// - If the left half (from 'low' to 'mid') is sorted, check if the target lies within that range.
-//   - If the target is within this range, update 'high' to mid - 1 to search the left half.
-//   - Otherwise, update 'low' to mid + 1 to search the right half.
-// - If the right half (from 'mid' to 'high') is sorted, check if the target is in this range.
-//   - If the target is within this range, update 'low' to mid + 1.
-//   - Otherwise, update 'high' to mid - 1 to search the left half.
-// continue this process until the target is found or 'low' exceeds 'high'.
-// TC:- O(LOGN), as binary search divides the array in half in each iteration.
-// SC:- O(1), since only a few pointers are used for the search.
+// i will use binary search and i will use two pointers 'low' and 'high' where low is initialized with 0  
+// and high is initialized with nums.length - 1.  
+// i will run a while loop until 'low <= high' and calculate the 'mid' index to check if the target is at mid.  
+// if the target is found at 'mid', return the mid index immediately.  
+// if not, then i will find out which part of the array is sorted (left or right) of mid.  
+// - if the left half (from 'low' to 'mid') is sorted, check if the target lies within that range, because the target will  
+//   be found in the sorted part of the array.  
+//   - if the target is within this range, update 'high' to mid - 1 to search the left half.  
+//   - otherwise, update 'low' to mid + 1 to search the right half.  
+// - if the right half (from 'mid' to 'high') is sorted, check if the target is in this range.  
+//   - if the target is within this range, update 'low' to mid + 1 to search the right half.  
+//   - otherwise, update 'high' to mid - 1 to search the left half.  
+// continue this process until the target is found or 'low' exceeds 'high'.  
+// if the target is not found, return -1.  
+// tc: O(log N), as binary search divides the array in half in each iteration.  
+// sc: O(1), since only a few pointers are used for the search. 
 
 var search = function(arr,target) {
 let low = 0;
