2c00ea8da8b8ebcdd435d5eb06871db334bbefc1
Dipali Bohara <dipalibohara500@gmail.com>
2025-05-29 13:34:50 +0530

    stack

--- stack_&_queue/valid_parentheses.js
+++ stack_&_queue/valid_parentheses.js
@@ -2,11 +2,14 @@
 // approach:-
 // i will use of stack to store corresponding closing parenthesis of the opening parenthesis 
 // and I will iterate through the given string and 
-// while iterating the string, when encountering an opening parenthesis ('(', '{', or '['), push the corresponding closing parenthesis (')', '}', or ']') onto the stack.
-// while iterating the strig if you encounter the closing parenthesis (')', '}', or ']'), check if it matches the top character of the stack. 
+// while iterating the string, when encountering an opening parenthesis ('(', '{', or '['), push the corresponding 
+// closing parenthesis (')', '}', or ']') onto the stack.
+// while iterating the strig if you encounter the closing parenthesis (')', '}', or ']'), check if it matches the top 
+// character of the stack. 
 // if it does, pop the top of the stack element. 
 // if it doesn't match or if the stack is empty return `false` immediately.
-// after processing all parentheses of the given string, if the stack is empty, it means all opening parentheses had matching closing parentheses, and the string is valid.
+// after processing all parenthesis of the given string, if the stack is empty, it means all opening parentheses 
+// had matching closing parentheses, and the string is valid.
 // If the stack is not empty, return `false` as there are unmatched opening parentheses.
 // TC: O(N), as we iterate through each character exactly once.
 // SC: O(N), due to the stack used to store the expected closing parentheses.
