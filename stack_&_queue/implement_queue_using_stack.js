ee955f0edbfe72774568bc6be1380204bbdaa026
Dipali Bohara <dipalibohara500@gmail.com>
2025-06-01 18:31:02 +0530

    stack

--- stack_&_queue/implement_queue_using_stack.js
+++ stack_&_queue/implement_queue_using_stack.js
@@ -9,7 +9,6 @@
 // Optimal Approach:
 // To implement a queue using stacks, I have used two stacks.
 // stack1 stores the elements as they come, while stack2 is used to perform queue-like behavior.
-
 // push method:- Adding an element to a queue follows the same process as adding an element to a stack.
 // So, I will simply push the value into stack1.
 
@@ -18,9 +17,13 @@
 // So, to remove the front value of the queue using two stacks, I will first move all elements from stack1 to stack2,
 // pop the top element from stack2 (which represents the front of the queue), and then move the remaining elements back to stack1.
 
-// peek method:- Retrieving the front element of the queue is similar to the pop operation.
-// I will move all elements from stack1 to stack2, get the top element of stack2 (which represents the front of the queue),
-// push it back into stack2, and then move all elements back to stack1.
+// peek method:- Getting the front element of the queue using stacks is different from using a regular queue.
+// In a stack, we remove elements from the top (LIFO), whereas in a queue, we access the front (FIFO),
+// which corresponds to the bottom of the stack.
+// So, to retrieve the front of the queue, I move all elements from stack1 to stack2,
+// read the top of stack2 (which represents the front of the queue), and then restore the elements back to stack1.
+// Unlike pop, peek does not remove the element — it just returns it.
+
 
 // isEmpty:- Just check if stack1 is empty.
 // If stack1.length is 0, it means the queue is empty; otherwise, it is not empty.
@@ -30,10 +33,10 @@
 //      (3) O(N) - Returning the front of the queue using a stack.
 //      (4) O(1) - Checking if the queue is empty.
 
-// MyStack is a constructor function in JavaScript. 
-// When we create a new stack using new MyStack(), JavaScript automatically creates an object for it.
+// MyQueue is a constructor function in JavaScript. 
+// When we create a new stack using new MyQueue(), JavaScript automatically creates an object for it.
 // Instead of adding methods like push, pop, top, and empty directly to each new object, JavaScript adds them to 
-// MyStack. prototype.
+// MyQueue. prototype.
 // This way, all stack objects share the same methods instead of each having its own copy, which saves memory. 
 // When we call a method on a stack object, JavaScript first looks for it in the object itself. If it’s not there,
 // it checks the prototype and finds the method there. This is how JavaScript allows multiple objects to use the same
