ff6f6728401bf01f4224a49cabfc5bfb84e12c0f
Dipali Bohara <dipalibohara500@gmail.com>
2025-06-02 14:00:42 +0530

    array

--- arrays/maximum_subarray.js
+++ arrays/maximum_subarray.js
@@ -1,5 +1,5 @@
 // Leetcode Problem:- 53
-/5 
+
 var maxSubArray = function(nums) {
     let longSub = -Infinity;
     for(let i = 0; i < nums.length; i++){
@@ -15,12 +15,16 @@ var maxSubArray = function(nums) {
 
 // Optimal Approach:
 // approach:
-// i will initialize a variable longSub with -Infinity to keep track of the maximum subarray sum found so far. Additionally, i will use of a sum variable initialized to 0 to compute the sum.
-// while iterating through the nums array, i will add the current element to sum. Meanwhile, i will update longSub with the maximum value between longSub and sum. 
-// if sum becomes negative, I will reset it to 0 if it becomes negative so that it doesn’t affect any future positive subarrays.
+// i will initialize a variable longSub with -Infinity to keep track of the maximum subarray sum found so far.
+// Additionally, i will use of a sum variable initialized to 0 to compute the sum.
+// while iterating through the nums array, i will add the current element to sum. Meanwhile, 
+// i will update longSub with the maximum value between longSub and sum. 
+// if sum becomes negative, I will reset it to 0 if it becomes negative so that it doesn’t affect any future positive
+// subarrays.
 // After iterating through all elements of nums, I will return longSub, which stores the maximum subarray sum.
 // TC:- O(N), to iterate through array nums linearly to find the sum.
 // SC:- O(1), since no additional space is used. 
+
 var maxSubArray = function(nums) {
     let longSub = -Infinity;
     let sum = 0;
