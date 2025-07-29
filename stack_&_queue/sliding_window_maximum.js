1e34f659576c6ec667b350acb32b5f487fa48b88
Dipali Bohara <dipalibohara500@gmail.com>
2025-07-29 18:44:25 +0530

    monotonic_dequeue_updated

--- stack_&_queue/sliding_window_maximum.js
+++ stack_&_queue/sliding_window_maximum.js
@@ -28,46 +28,67 @@ var maxSlidingWindow = function(nums, k) {
     return arr;
 };
 
-// Optimal Approach: Using 'monotonic dequeue'
-// Approach:-
-// use a 'dequeue' to store indices of useful elements in the current window. 
-// This deque will maintain a decreasing order of values from front to back, 
-// ensuring the maximum element of the window is always at the front of the deque.
-// use an array 'result' to store the maximum values for each window of size 'k'.
+// Optimal Approach: Using 'Monotonic Deque'
+// Approach:
+// Use a 'deque' to store indices of useful elements in the current window that are part of current window.
+// This deque will maintain a decreasing order of values from front to back,
+// ensuring that the maximum element of the window is always at the front of the deque.
+// Use an array 'result' to store the maximum values for each window of size 'k'.
+
 // While iterating through the array 'nums', perform the following operations:
-//  - remove indices of elements that are out of the current window (i.e., elements whose 
-//     index is less than 'i - k + 1') from the front of the deque.
-//  - remove indices of all elements from the back of the deque that are smaller than the 
-//     current element 'nums[i]', because they are no longer useful.
-//  - add the current element's index at the back of the deque. 
-//  - once the first window is processed (i.e., when 'i >= k - 1'), 
-//     add the element at the front of the deque (which is the maximum in the current window) to the 'result' array.
-// TC:- O(N), where 'N' is the length of 'nums', as each element is added and removed from the deque at most once.
-// SC:- O(k), to store indices of elements in the deque (with a maximum of 'k' elements at any time).
+//  (i) Remove indices from the deque that are out of the current window (i.e., indices less than 'i - k + 1') from the
+//    front of the deque.
+//    i - k + 1 is the starting index of the current window.
+//    In deque[0], if the index is less than the starting index of the current window, we delete it —
+//    since we store indices in the deque from the back and the index at the front of the deque is the oldest index.
+//  (ii) Remove indices of all elements from the back of the deque that are smaller than the
+//    current element 'nums[i]', since we are using a monotonic **decreasing** deque
+//    (the front of the deque should always contain the maximum value).
+//  (iii) Add the current element's index to the back of the deque, since we're traversing the array from left to right.
+//  (iv) whenever we found the window (i.e., when 'i >= k - 1'),
+//    add the element at the front of the deque (which is the maximum in the current window) to the 'result' array.
+
+// Time Complexity: O(N), where 'N' is the length of 'nums', as each element is added and removed from the deque at most
+//  once.
+// Space Complexity: O(k), to store indices of elements in the deque (with a maximum of 'k' elements at any time).
+
+// Note:
+// What is a Deque?
+// A Deque stands for Double-Ended Queue.
+// It is a linear data structure that allows you to insert and delete elements from both the front and the rear (end).
+
+// What is a Monotonic Deque?
+// A monotonic deque is a specialized form of double-ended queue (deque) where the elements are maintained in either
+// increasing or decreasing order And here in this problem i have dequeue in decreasing order since i am iterating array
+// from left to right.
+// It is commonly used in algorithmic problems that involve efficiently tracking the maximum or minimum values in a
+// sliding window or range.
+// The deque is updated in such a way that irrelevant elements (that can no longer affect the result) are removed,
+// maintaining only the potential candidates for the min or max.
 
 var maxSlidingWindow = function(nums, k) {
     let n = nums.length;
-    let dequeue = [];  
+    let deque = [];  
     let result = [];   
 
     for (let i = 0; i < n; i++) {
-        // Remove indices of elements from dequeue that are not a part of current window
-        if (dequeue.length > 0 && dequeue[0] < i - k + 1) {
-            dequeue.shift();
+        // Remove indices from the front of the deque that are not a part of the current window
+        if (deque.length > 0 && deque[0] < i - k + 1) {
+            deque.shift();
         }
 
-        // Remove indices of all elements from dequeue which are smaller than current element (nums[i])
-        // from the back of the dequeue, because they are not useful.
-        while (dequeue.length > 0 && nums[dequeue[dequeue.length - 1]] < nums[i]) {
-            dequeue.pop();
+        // Remove indices of all elements from the deque that are smaller than the current element (nums[i])
+        // from the back of the deque, because they are not useful.
+        while (deque.length > 0 && nums[deque[deque.length - 1]] < nums[i]) {
+            deque.pop();
         }
 
-        // Add the current element index at the back of the dequeue.
-        dequeue.push(i);
+        // Add the current element index to the back of the deque.
+        deque.push(i);
 
-        // Element at the front of the dequeue is the largest in the current window, so add that in 'results' array.
+        // Element at the front of the deque is the largest in the current window, so add that to the 'result' array.
         if (i >= k - 1) {
-            result.push(nums[dequeue[0]]);
+            result.push(nums[deque[0]]);
         }
     }
 
