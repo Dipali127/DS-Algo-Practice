3b8514d1d6cb0615dd7e5ed3d26a1a629fd060f3
Dipali Bohara <dipalibohara500@gmail.com>
2025-12-07 23:47:00 +0530

    bit manipulation

--- bit_manipulation/sum_of_two_integers.js
+++ bit_manipulation/sum_of_two_integers.js
@@ -10,13 +10,18 @@ var getSum = function(a, b) {
 // Next, I will compute the carry using the AND (&) operator, followed by a left shift (<<) operator,
 // which will give the carry bits that is required for next step addition.
 // In each step, the carry is reduced, and the process continues until the carry becomes zero.
-// I will continue this process by updating 'a' with the partial sum and 'b' with the carry until the carry (b) becomes 0.
+// I will continue this process by updating 'a' with the partial sum and 'b' with the carry until the carry (b)
+//  becomes 0.
 // once the carry is 0, the value of 'a' will hold the sum of the two numbers.
 // This approach simulates the process of adding two numbers bit by bit without using the + or - operators.
-// TC:- O(LOG(max(a,b))), as the number of steps depends on the number of bits required to represent the larger of the 
-// two numbers, a or b. 
-// Since the carry shrinks with each iteration, the number of iterations is proportional to the bit length
-// of the larger number. 
+// TC: O(log(max(a, b))) because in each iteration, the carry is shifted one bit to the left. 
+// The number of steps depends on the number of bits required to represent the larger of 
+// the two numbers, a or b. 
+// Since the carry shrinks with each iteration, the number of iterations is proportional 
+// to the bit length of the larger number.
+// Note:- here, I am not running a linear loop. Instead, I use a while loop that runs until the carry becomes 
+// zero. The number of iterations depends on how many bit shifts are needed to eliminate the carry by shifting 
+// it to the left. Once the carry becomes zero, the loop stops.
 // SC:- O(1), since no additional space is required.
 
 var getSum = function(a, b){
