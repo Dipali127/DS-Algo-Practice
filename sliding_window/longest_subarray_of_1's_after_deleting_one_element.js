a7e71bd34f4469a192789df0455e1110f7046226
Dipali Bohara <dipalibohara500@gmail.com>
2026-01-11 23:05:54 +0530

    updated sliding window

--- sliding_window/longest_subarray_of_1's_after_deleting_one_element.js
+++ sliding_window/longest_subarray_of_1's_after_deleting_one_element.js
@@ -1,16 +1,17 @@
 // Leetcode Problem: 1493
 // Brute Force Approach:
 // Approach:
-// Consider each possible subarray, and for each subarray take a variable `countZero` initially initialized to 0 
-// to keep track of the number of zeroes in the current subarray.
+// Consider each possible subarray, and for each subarray take a variable `countZero` initially 
+// initialized to 0 to keep track of the number of zeroes in the current subarray.
 // Iterate through the current subarray, and while iterating:
 // Check if the current number in the subarray is 0. If it is, increment `countZero` by 1.
 // Then check if `countZero` is greater than 1. If so, break the current subarray loop, 
 // since we are allowed to remove only one element from the array (as per the given problem).
 // But if `countZero` is less than or equal to 1, then update `maxLength` with the maximum 
-// between `maxLength` and `j - i`, where `j - i` gives the length of the current subarray after removing one element
-// (since we need to delete one element).
-// Time Complexity: O(N^2), since we use a nested loop to calculate the longest subarray of 1's after deleting one element.
+// between `maxLength` and `j - i`, where `j - i` gives the length of the current subarray after removing
+//  one element (since we need to delete one element).
+// Time Complexity: O(N^2), since we use a nested loop to calculate the longest subarray of 1's after 
+// deleting one element.
 // Space Complexity: O(1), as no additional space is used apart from a few variables.
 
 var longestSubarray = function(nums){
@@ -38,12 +39,12 @@ var longestSubarray = function(nums){
 // approach:
 // extend the window by moving the pointer 'j' to the right of the array 'nums'. 
 // and check if the value at 'nums[j]' is zero, increment the 'countZero'.
-// shrink the window if 'countZero' is greater than 1 (i.e., there are more than one zero in the current window),
-// move the pointer 'i' to the right until 'countZero' becomes 1. it ensures that the window contains at most one
-// zero, which is valid for finding the longest subarray of 1's after deleting one zero because that one zero 
-// again deleted from current window while computing length using j - i.
-// for each position of 'j', calculate the length of the current valid window as 'j - i' which gives the exact 
-// number of 1's in the current window after removing one element (i.e., the zero).
+// shrink the window if 'countZero' is greater than 1 (i.e., there are more than one zero in the current
+//  window), move the pointer 'i' to the right until 'countZero' becomes 1. it ensures that the window 
+// contains at most one zero, which is valid for finding the longest subarray of 1's after deleting one 
+// zero because that one zero again deleted from current window while computing length using j - i.
+// for each position of 'j', calculate the length of the current valid window as 'j - i' which gives the 
+// exact number of 1's in the current window after removing one element (i.e., the zero).
 // update 'maxLength' with the maximum value between the current 'maxLength' and the length of the current window(j-i). 
 // TC:- O(N), since we traverse the array once with two pointers (i and j).
 // SC:- O(1), since no additional space is used other than the few variables.
